369c9f6c35914059b3ffc84424a4119ef80dfd27
Nigel Grange <nigel.grange@wildpalm.co.uk>
2017-02-14 14:27:04 +0000

    Product overview collection view mechanics

---
+++ JLProjectFeb17/JLProjectFeb17/ProductOverviewCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductOverviewCollectionViewCell.swift
+//  JLProjectFeb17
+//
+//  Created by Nigel Grange on 14/02/2017.
+//  Copyright © 2017 Nigel Grange. All rights reserved.
+//
+
+import UIKit
+
+class ProductOverviewCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- JLProjectFeb17/JLProjectFeb17/ViewController.swift
+++ JLProjectFeb17/JLProjectFeb17/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct CollectionViewConstants {
+    static let ReuseIdentifier = "ProductOverviewCell"
+}
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.register(UINib.init(nibName: "ProductOverviewCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: CollectionViewConstants.ReuseIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3;
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewConstants.ReuseIdentifier, for: indexPath)
+        return cell
+    }
 
 }
 
